2be1afa77050e4069c49144a2e8fe143872154ec
auraakart <auraakart@gmail.com>
2026-08-30 13:41:42 +0530

    feat(api): register Prisma service

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller('health')
 class HealthController {
@@ -8,5 +9,9 @@ class HealthController {
   }
 }
 
-@Module({ controllers: [HealthController] })
+@Module({
+  controllers: [HealthController],
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
 export class AppModule {}
