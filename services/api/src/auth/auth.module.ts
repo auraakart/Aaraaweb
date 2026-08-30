c745d2072449993e1e175526fcd9af49a0590f34
auraakart <auraakart@gmail.com>
2026-08-30 22:03:59 +0530

    feat(auth): wire session and bearer guards

--- services/api/src/auth/auth.module.ts
+++ services/api/src/auth/auth.module.ts
@@ -2,11 +2,14 @@ import { Global, Module } from '@nestjs/common';
 import { RolesGuard } from './roles.guard';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { OtpService } from './otp.service';
+import { SessionService } from './session.service';
+import { BearerGuard } from './bearer.guard';
 
 @Global()
 @Module({
   controllers: [AuthController],
-  providers: [RolesGuard, AuthService],
-  exports: [RolesGuard, AuthService],
+  providers: [RolesGuard, AuthService, OtpService, SessionService, BearerGuard],
+  exports: [RolesGuard, AuthService, OtpService, SessionService, BearerGuard],
 })
 export class AuthModule {}
