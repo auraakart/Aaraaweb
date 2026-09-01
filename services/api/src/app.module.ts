a7a4947aa7553d3f74015d1ad97967b28946dbb8
auraakart <auraakart@gmail.com>
2026-09-01 18:37:32 +0530

    feat: register tenant entitlements foundation

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { EntitlementsModule } from './entitlements/entitlements.module';
 import { ResidentsModule } from './residents/residents.module';
 import { SocietiesModule } from './societies/societies.module';
 import { PropertiesModule } from './properties/properties.module';
@@ -16,7 +17,7 @@ class HealthController {
 }
 
 @Module({
-  imports: [AuthModule, SocietiesModule, ResidentsModule, PropertiesModule, GatesModule, VisitorsModule],
+  imports: [AuthModule, EntitlementsModule, SocietiesModule, ResidentsModule, PropertiesModule, GatesModule, VisitorsModule],
   controllers: [HealthController],
   providers: [PrismaService],
   exports: [PrismaService],
