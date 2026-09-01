82ca69dce084a2a37c8a1c01d8e808ad38cbecde
auraakart <auraakart@gmail.com>
2026-09-01 18:37:08 +0530

    feat: wire visitor feature entitlements

--- services/api/src/visitors/visitors.module.ts
+++ services/api/src/visitors/visitors.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { GatesModule } from '../gates/gates.module';
 import { VisitorsController } from './visitors.controller';
 import { VisitorService } from './visitor.service';
 import { VisitorVerificationService } from './visitor-verification.service';
 
 @Module({
-  imports: [GatesModule],
+  imports: [GatesModule, EntitlementsModule],
   controllers: [VisitorsController],
   providers: [PrismaService, VisitorService, VisitorVerificationService],
   exports: [VisitorService, VisitorVerificationService],
