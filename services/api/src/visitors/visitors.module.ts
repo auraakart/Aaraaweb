0c754ee81d6c3eb8be762d9c23a31853e4a56e72
auraakart <auraakart@gmail.com>
2026-09-01 18:13:55 +0530

    fix(visitors): import gate audit dependencies

--- services/api/src/visitors/visitors.module.ts
+++ services/api/src/visitors/visitors.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { GatesModule } from '../gates/gates.module';
 import { VisitorsController } from './visitors.controller';
 import { VisitorService } from './visitor.service';
 import { VisitorVerificationService } from './visitor-verification.service';
 
 @Module({
+  imports: [GatesModule],
   controllers: [VisitorsController],
   providers: [PrismaService, VisitorService, VisitorVerificationService],
   exports: [VisitorService, VisitorVerificationService],
