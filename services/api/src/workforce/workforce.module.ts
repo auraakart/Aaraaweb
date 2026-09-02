257024551ab7fbe1be80b44a94f2b4e3224df790
auraakart <auraakart@gmail.com>
2026-09-02 07:08:13 +0530

    Wire workforce gate attendance dependencies

--- services/api/src/workforce/workforce.module.ts
+++ services/api/src/workforce/workforce.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { AccessModule } from '../access/access.module';
 import { EntitlementsModule } from '../entitlements/entitlements.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { PrismaService } from '../prisma/prisma.service';
 import { WorkforceController } from './workforce.controller';
 import { WorkforceService } from './workforce.service';
 
 @Module({
-  imports: [EntitlementsModule],
+  imports: [EntitlementsModule, AccessModule, NotificationsModule],
   controllers: [WorkforceController],
   providers: [PrismaService, WorkforceService],
   exports: [WorkforceService],
