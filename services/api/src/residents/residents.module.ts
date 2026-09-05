681fd22e1fdd27fa9b65108232252b3311b6f21a
auraakart <auraakart@gmail.com>
2026-09-05 21:51:18 +0530

    Register society role management controller

--- services/api/src/residents/residents.module.ts
+++ services/api/src/residents/residents.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ResidentsController } from './residents.controller';
+import { SocietyRolesController } from './society-roles.controller';
 
-@Module({ controllers: [ResidentsController], providers: [PrismaService], exports: [PrismaService] })
+@Module({ controllers: [ResidentsController, SocietyRolesController], providers: [PrismaService], exports: [PrismaService] })
 export class ResidentsModule {}
