b25377112054e125b0a6af749b88b5d290ae5f7b
auraakart <auraakart@gmail.com>
2026-09-05 22:10:45 +0530

    Register marketplace operations service

--- services/api/src/services-marketplace/services-marketplace.module.ts
+++ services/api/src/services-marketplace/services-marketplace.module.ts
@@ -4,12 +4,13 @@ import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { ServicesMarketplaceController } from './services-marketplace.controller';
 import { ServicesPlatformController } from './services-platform.controller';
+import { ServicesMarketplaceOperationsService } from './services-marketplace-operations.service';
 import { ServicesMarketplaceService } from './services-marketplace.service';
 
 @Module({
   imports: [AccessModule, EntitlementsModule],
   controllers: [ServicesMarketplaceController, ServicesPlatformController],
-  providers: [PrismaService, ServicesMarketplaceService],
+  providers: [PrismaService, ServicesMarketplaceOperationsService, ServicesMarketplaceService],
   exports: [ServicesMarketplaceService],
 })
 export class ServicesMarketplaceModule {}
