c2c9c52b2ec819a88a47da844c27ea19358235f4
auraakart <auraakart@gmail.com>
2026-09-09 23:48:56 +0530

    fix: wire amenities feature guard dependencies

--- services/api/src/amenities/amenities.module.ts
+++ services/api/src/amenities/amenities.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { EntitlementsModule } from '../entitlements/entitlements.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { AmenitiesController } from './amenities.controller';
 import { AmenitiesService } from './amenities.service';
 
 @Module({
+  imports: [EntitlementsModule],
   controllers: [AmenitiesController],
   providers: [AmenitiesService, PrismaService],
 })
