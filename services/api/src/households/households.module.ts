ca5b876a4c217155ee0593b5176e08fd8c5b55ce
auraakart <auraakart@gmail.com>
2026-09-10 08:51:19 +0530

    fix: register household preference guard service

--- services/api/src/households/households.module.ts
+++ services/api/src/households/households.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { HouseholdChangeRequestService } from './household-change-request.service';
+import { HouseholdPreferencesService } from './household-preferences.service';
 import { HouseholdService } from './household.service';
 import { HouseholdsController } from './households.controller';
 import { ParkingAdminController } from './parking-admin.controller';
 
 @Module({
   controllers: [HouseholdsController, ParkingAdminController],
-  providers: [PrismaService, HouseholdService, HouseholdChangeRequestService],
-  exports: [HouseholdService, HouseholdChangeRequestService],
+  providers: [PrismaService, HouseholdService, HouseholdChangeRequestService, HouseholdPreferencesService],
+  exports: [HouseholdService, HouseholdChangeRequestService, HouseholdPreferencesService],
 })
 export class HouseholdsModule {}
