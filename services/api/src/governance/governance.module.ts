c65edb54f288c73d7372983244494b5585f5c195
auraakart <auraakart@gmail.com>
2026-09-13 04:57:04 +0530

    feat(governance): register artifacts controller

--- services/api/src/governance/governance.module.ts
+++ services/api/src/governance/governance.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { GovernanceArtifactsController } from './governance-artifacts.controller';
 import { GovernanceController } from './governance.controller';
 import { GovernanceService } from './governance.service';
 
 @Module({
-  controllers:[GovernanceController],
+  controllers:[GovernanceController,GovernanceArtifactsController],
   providers:[GovernanceService,PrismaService],
   exports:[GovernanceService],
 })
