afac440ee4e23b805ec2c443fc3fa43034a92635
auraakart <auraakart@gmail.com>
2026-09-12 19:05:57 +0530

    feat(accounting): register settlement API

--- services/api/src/accounting/accounting.module.ts
+++ services/api/src/accounting/accounting.module.ts
@@ -5,11 +5,13 @@ import { AccountingController } from './accounting.controller';
 import { AccountingService } from './accounting.service';
 import { ReceivablesController } from './receivables.controller';
 import { ReceivablesService } from './receivables.service';
+import { SettlementController } from './settlement.controller';
+import { SettlementService } from './settlement.service';
 
 @Module({
   imports: [EntitlementsModule],
-  controllers: [AccountingController, ReceivablesController],
-  providers: [AccountingService, ReceivablesService, PrismaService],
-  exports: [AccountingService, ReceivablesService],
+  controllers: [AccountingController, ReceivablesController, SettlementController],
+  providers: [AccountingService, ReceivablesService, SettlementService, PrismaService],
+  exports: [AccountingService, ReceivablesService, SettlementService],
 })
 export class AccountingModule {}
