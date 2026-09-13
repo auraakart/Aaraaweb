c8c7a0124620751a688d9b14fb34fbabc1cbce56
auraakart <auraakart@gmail.com>
2026-09-13 06:21:53 +0530

    feat(facilities): wire facilities module into API

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AmenitiesModule } from './amenities/amenities.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { EntitlementsModule } from './entitlements/entitlements.module';
+import { FacilitiesModule } from './facilities/facilities.module';
 import { GovernanceModule } from './governance/governance.module';
 import { HelpdeskModule } from './helpdesk/helpdesk.module';
 import { NoticesModule } from './notices/notices.module';
@@ -40,6 +41,7 @@ import { RequestObservabilityMiddleware } from './observability/request-observab
     VisitorsModule,
     AccessModule,
     AmenitiesModule,
+    FacilitiesModule,
     ServicesMarketplaceModule,
     BillingModule,
     AccountingModule,
