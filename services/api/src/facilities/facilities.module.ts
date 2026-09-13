8426791941ed23429ca0b4cb838d8b5350d793ae
auraakart <auraakart@gmail.com>
2026-09-13 07:07:17 +0530

    feat(facilities): wire operational alerts

--- services/api/src/facilities/facilities.module.ts
+++ services/api/src/facilities/facilities.module.ts
@@ -3,6 +3,8 @@ import { PrismaService } from '../prisma/prisma.service';
 import { FacilitiesController } from './facilities.controller';
 import { FacilitiesPreventiveController } from './facilities-preventive.controller';
 import { FacilitiesContractsController } from './facilities-contracts.controller';
+import { FacilitiesAlertsController } from './facilities-alerts.controller';
+import { FacilitiesAlertsService } from './facilities-alerts.service';
 
-@Module({controllers:[FacilitiesController,FacilitiesPreventiveController,FacilitiesContractsController],providers:[PrismaService]})
+@Module({controllers:[FacilitiesController,FacilitiesPreventiveController,FacilitiesContractsController,FacilitiesAlertsController],providers:[PrismaService,FacilitiesAlertsService]})
 export class FacilitiesModule{}
