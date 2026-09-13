48f3083651ae47f312eaf6e1894bd25a9bdc495f
auraakart <auraakart@gmail.com>
2026-09-13 06:28:44 +0530

    feat(facilities): register preventive maintenance controller

--- services/api/src/facilities/facilities.module.ts
+++ services/api/src/facilities/facilities.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { FacilitiesController } from './facilities.controller';
+import { FacilitiesPreventiveController } from './facilities-preventive.controller';
 
-@Module({controllers:[FacilitiesController],providers:[PrismaService]})
+@Module({controllers:[FacilitiesController,FacilitiesPreventiveController],providers:[PrismaService]})
 export class FacilitiesModule{}
