924d4cf3580cc75f1d32e4351bf50171fdcaeba2
auraakart <auraakart@gmail.com>
2026-09-15 03:51:48 +0530

    feat(utilities): register invoice issuance

--- services/api/src/utilities/utilities.module.ts
+++ services/api/src/utilities/utilities.module.ts
@@ -4,10 +4,12 @@ import { UtilitiesController } from './utilities.controller';
 import { UtilitiesService } from './utilities.service';
 import { UtilityChargesController } from './utility-charges.controller';
 import { UtilityChargesService } from './utility-charges.service';
+import { UtilityInvoicesController } from './utility-invoices.controller';
+import { UtilityInvoicesService } from './utility-invoices.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UtilitiesController, UtilityChargesController],
-  providers: [UtilitiesService, UtilityChargesService],
+  controllers: [UtilitiesController, UtilityChargesController, UtilityInvoicesController],
+  providers: [UtilitiesService, UtilityChargesService, UtilityInvoicesService],
 })
 export class UtilitiesModule {}
