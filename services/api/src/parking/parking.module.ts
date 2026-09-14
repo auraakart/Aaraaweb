e3b8adeb7e482bf51c2761e89867f10b593bf839
auraakart <auraakart@gmail.com>
2026-09-14 23:19:55 +0530

    feat(parking): register visitor permit service

--- services/api/src/parking/parking.module.ts
+++ services/api/src/parking/parking.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ParkingController } from './parking.controller';
+import { ParkingPermitService } from './parking-permit.service';
 import { ParkingService } from './parking.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ParkingController],
-  providers: [ParkingService],
+  providers: [ParkingService, ParkingPermitService],
 })
 export class ParkingModule {}
