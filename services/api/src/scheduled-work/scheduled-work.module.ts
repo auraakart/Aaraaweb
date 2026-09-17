356bc0f19339fe4109b4acd320c0b4158aad9fb5
auraakart <auraakart@gmail.com>
2026-09-17 21:46:46 +0530

    feat: run durable object storage cleanup in scheduled work

--- services/api/src/scheduled-work/scheduled-work.module.ts
+++ services/api/src/scheduled-work/scheduled-work.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ObjectStorageModule } from '../storage/object-storage.module';
+import { ObjectStorageCleanupService } from './object-storage-cleanup.service';
 import { ScheduledWorkService } from './scheduled-work.service';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [ScheduledWorkService],
-  exports: [ScheduledWorkService],
+  imports: [PrismaModule, ObjectStorageModule],
+  providers: [ScheduledWorkService, ObjectStorageCleanupService],
+  exports: [ScheduledWorkService, ObjectStorageCleanupService],
 })
 export class ScheduledWorkModule {}
