8bad7f1209755d12b9d3258ff34668ae51bdf1b2
auraakart <auraakart@gmail.com>
2026-09-18 22:31:33 +0530

    test(api): cover vendor contract history authorization

--- services/api/src/vendors/vendor-contracts.authorization.spec.ts
+++ services/api/src/vendors/vendor-contracts.authorization.spec.ts
@@ -4,6 +4,6 @@ import { PERMISSIONS_KEY } from '../auth/permissions.decorator';
 import { VendorContractsController } from './vendor-contracts.controller';
 
 describe('VendorContractsController authorization',()=>{
- it('list requires SOCIETY_VENDORS_READ',()=>expect(Reflect.getMetadata(PERMISSIONS_KEY,VendorContractsController.prototype.list)).toEqual([AppPermission.SOCIETY_VENDORS_READ]));
+ for(const method of ['list','history'] as const){it(`${method} requires SOCIETY_VENDORS_READ`,()=>expect(Reflect.getMetadata(PERMISSIONS_KEY,VendorContractsController.prototype[method])).toEqual([AppPermission.SOCIETY_VENDORS_READ]));}
  for(const method of ['create','status'] as const){it(`${method} requires SOCIETY_VENDORS_MANAGE`,()=>expect(Reflect.getMetadata(PERMISSIONS_KEY,VendorContractsController.prototype[method])).toEqual([AppPermission.SOCIETY_VENDORS_MANAGE]));}
 });
