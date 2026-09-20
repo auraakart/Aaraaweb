1c085aa71e4d314a2a277fdd2c9c2d507eb437ec
auraakart <auraakart@gmail.com>
2026-09-20 15:00:44 +0530

    test(integrations): cover configuration permissions

--- services/api/src/integrations/integration-registry.authorization.spec.ts
+++ services/api/src/integrations/integration-registry.authorization.spec.ts
@@ -4,8 +4,15 @@ import { PERMISSIONS_KEY } from '../auth/permissions.decorator';
 import { IntegrationRegistryController } from './integration-registry.controller';
 
 describe('IntegrationRegistryController authorization', () => {
-  it('requires society configuration read permission', () => {
-    expect(Reflect.getMetadata(PERMISSIONS_KEY, IntegrationRegistryController.prototype.list))
-      .toEqual([AppPermission.SOCIETY_CONFIGURATION_READ]);
+  for (const method of ['list','configurationList','configurationEvents'] as const) {
+    it(`${method} requires society configuration read permission`, () => {
+      expect(Reflect.getMetadata(PERMISSIONS_KEY, IntegrationRegistryController.prototype[method]))
+        .toEqual([AppPermission.SOCIETY_CONFIGURATION_READ]);
+    });
+  }
+
+  it('updateConfiguration requires society configuration manage permission', () => {
+    expect(Reflect.getMetadata(PERMISSIONS_KEY, IntegrationRegistryController.prototype.updateConfiguration))
+      .toEqual([AppPermission.SOCIETY_CONFIGURATION_MANAGE]);
   });
 });
